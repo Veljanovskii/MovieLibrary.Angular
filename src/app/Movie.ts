1dadc41d5b9845d07cc1284d6fa8695594962c9f
Milos Veljanovski <milosveljanovski@gmail.com>
2021-12-29 10:46:14 +0100

    feat: add service, components & material

---
+++ src/app/Movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  MovieId: number;
+  Caption: string;
+  ReleaseYear: number;
+  MovieLength: number;
+  InsertDate: Date;
+  DeleteDate: Date;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { MoviesComponent } from './movies/movies.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
