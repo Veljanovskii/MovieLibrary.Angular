5b81bc92e29a77827e6ba46d81d3ab3db31afb58
Milos Veljanovski <milosveljanovski@gmail.com>
2022-01-05 21:06:00 +0100

    feat: movie search done, working on add movie

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SortDirection } from '@angular/material/sort';
-import { catchError, Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { Movie } from './Movie';
 import { MoviesCount } from './MoviesCount';
 
 @Injectable({
@@ -15,10 +16,19 @@ export class MovieService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  getMovies(sort: string, order: SortDirection, page: number, size:number): Observable<MoviesCount> {
+  getMovies(sort: string, order: SortDirection, page: number, size:number, search:string): Observable<MoviesCount> {
     const href = 'https://localhost:44371/api/Movie';
-    const requestUrl = `${href}?sort=${sort}&order=${order}&page=${page}&size=${size}`;
+    const requestUrl = `${href}?sort=${sort}&order=${order}&page=${page}&size=${size}&search=${search}`;
 
     return this._httpClient.get<MoviesCount>(requestUrl);
+  }
+
+  addMovie(movie: Movie): Observable<Movie> {
+    const href = 'https://localhost:44371/api/Movie';
+
+    movie.insertDate = new Date();
+    console.warn(movie);
+
+    return this._httpClient.post<Movie>(href, movie, this.httpOptions);
   }
 }
